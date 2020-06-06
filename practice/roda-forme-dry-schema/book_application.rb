97703be138fba28ec34de1cf669dae6f9617dca1
ksutikm <tsksusha@yandex.ru>
2020-06-06 03:13:28 +0300

    Add circulation to the book parameters

--- practice/roda-forme-dry-schema/book_application.rb
+++ practice/roda-forme-dry-schema/book_application.rb
@@ -26,21 +26,24 @@ class BookApplication < Roda
         title: 'Идиот',
         author: 'Лев Толстой',
         published_on: Date.parse('2013-06-18'),
-        mark: 5
+        mark: 5,
+        circulation: 3.5
       ),
       Book.new(
         id: 7,
         title: 'Божественная комедия',
         author: 'Данте Алигьери',
         published_on: Date.parse('2000-05-20'),
-        mark: 4
+        mark: 4,
+        circulation: 9
       ),
       Book.new(
         id: 2,
         title: 'Заратустра',
         author: 'Фридрих Ницше',
         published_on: Date.parse('1935-07-08'),
-        mark: 5
+        mark: 5,
+        circulation: 15
       )
     ]
   )
